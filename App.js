df519afc94b49afadbd344f41ea923933fb4ddf6
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-08-25 08:34:52 -0600

    welcome screen completed

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
 import React from "react";
 import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
 
   return (
-    <View style={{
-      backgroundColor: 'lightblue',
-      flex: 1 ,
-    }} >
-       <View style={{
-      backgroundColor: 'lightblue',
-      flex: 2 , 
-    }} />
-    <View style={{
-      backgroundColor: 'gold',
-      flex: 1 ,
-    }} />
-    <View style={{
-      backgroundColor: 'tomato',
-      flex: 1 ,
-    }} />
-</View> 
+    <WelcomeScreen/>
   );
 }
